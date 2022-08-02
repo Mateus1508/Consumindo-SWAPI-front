abcece5c4ff12b26cd1366a7ec2d7258258fe3a1
Mateus <81775179+Mateus1508@users.noreply.github.com>
2022-08-02 09:23:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Star Wars Wiki</title>
     <link rel="stylesheet" href="style.css">
 <!-- Fonts -->
     <link href="//db.onlinewebfonts.com/c/0c724f6aa457310440cf8949c615cbd7?family=Star+Jedi" rel="stylesheet" type="text/css"/>
